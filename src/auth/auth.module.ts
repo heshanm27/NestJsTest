e10d34422e38d65fcb234eb3e0c3cf65f50f5cde
B.D.Heshan Madhuranga <39455525+heshanm27@users.noreply.github.com>
2023-01-16 10:49:02 +0530

    Update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,18 +3,17 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
 import { PassportModule } from '@nestjs/passport';
-// import { JwtModule } from '@nestjs/jwt/dist';
-// import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtModule } from '@nestjs/jwt/dist';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-  // providers: [AuthService,JwtStrategy,LocalStrategy],
+  providers: [AuthService,JwtStrategy,LocalStrategy],
   controllers: [AuthController],
-  // imports: [UserModule,PassportModule.register({defaultStrategy: 'jwt'}),JwtModule.register({
-  //   secret: 'secretKey',
-  //   signOptions: { expiresIn: '3600s' },
-  // })],
-  imports:[UserModule,PassportModule],
-  providers: [AuthService, LocalStrategy],
+  imports: [UserModule,PassportModule.register({defaultStrategy: 'jwt'}),JwtModule.register({
+    secret: 'secretKey',
+    signOptions: { expiresIn: '3600s' },
+  })],
+
 })
 export class AuthModule {}

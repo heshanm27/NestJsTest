23f25afd5bf2105b42028f68b2033cc264b2fcc1
B.D.Heshan Madhuranga <39455525+heshanm27@users.noreply.github.com>
2023-01-17 18:18:07 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as passport from "passport"
-import {ValidationPipe} from "@nestjs/common"
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
   app.useGlobalPipes(new ValidationPipe());
-
   await app.listen(8000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }

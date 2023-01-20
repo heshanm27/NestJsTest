c0d33fddf755f959706e59c359512525cdd0e7ed
B.D.Heshan Madhuranga <39455525+heshanm27@users.noreply.github.com>
2023-01-20 08:58:20 +0530

    update test

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -4,12 +4,22 @@ import { UserService } from 'src/user/user.service';
 import { AuthService } from './auth.service';
 import { User } from 'src/user/entity/user.entity';
 import { UserCreateDto } from 'src/user/dto/usercreate.dto';
+import * as httpMocks from 'node-mocks-http';
 
 describe('Auth Controller', () => {
   let controller: AuthController;
+  const mockRequest = httpMocks.createRequest();
+  mockRequest.user = {
+    id: 'Id1',
+    email: 'test@gmail.com',
+    password: '123456',
+    firstName: 'test',
+    lastName: 'testlastname',
+    role: 'reader',
+  };
   const mockAuthService = {
     login: jest.fn((user: User) => {
-      return { access_token: 'test' };
+      return Promise.resolve({ access_token: 'test' });
     }),
     signUp: jest.fn((user: UserCreateDto) =>
       Promise.resolve({
@@ -34,5 +44,9 @@ describe('Auth Controller', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return access_token', async () => {
+    const result = await controller.login(mockRequest);
   });
 });

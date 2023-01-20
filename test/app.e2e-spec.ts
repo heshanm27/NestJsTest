8db9a79f17d21142ef367dab0b5f70405dfd5912
B.D.Heshan Madhuranga <39455525+heshanm27@users.noreply.github.com>
2023-01-20 14:26:44 +0530

    Jest Array Contain Check

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -7,11 +7,12 @@ import { Repository } from 'typeorm';
 import { Post } from '../src/post/entity/post.entity';
 import { User } from '../src/user/entity/user.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { PostCreateDto } from 'src/post/dto/postCreate.dto';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
   let BASE_URL: string = 'http://localhost:8000';
-
+  let token = '';
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -27,8 +28,9 @@ describe('AppController (e2e)', () => {
     const postRepo = app.get<Repository<Post>>(getRepositoryToken(Post));
     const userRep = app.get<Repository<User>>(getRepositoryToken(User));
     await userRep.delete({});
-    await postRepo.delete({});
+    // await postRepo.delete({});
     await app.close();
+    console.log('App closed');
   });
 
   describe('Auth', () => {
@@ -97,5 +99,115 @@ describe('AppController (e2e)', () => {
         });
       });
     });
+
+    describe('signin', () => {
+      it('should throw an error if email empty', async () => {
+        const userDto: UserCreateDto = {
+          email: '',
+          password: '123456',
+        };
+        const reponse = await await request(BASE_URL)
+          .post('/auth/login')
+          .send(userDto);
+
+        console.log(reponse.body);
+        expect(reponse.statusCode).toBe(401);
+        expect(reponse.body).toHaveProperty('message');
+        expect(reponse.body.message).toContain('Unauthorized');
+      });
+
+      it('should throw an error if password wrong', async () => {
+        const userDto: UserCreateDto = {
+          email: 'test@gmail.com',
+          password: '1234562',
+        };
+        const reponse = await await request(BASE_URL)
+          .post('/auth/login')
+          .send(userDto);
+        console.log(reponse.body);
+        expect(reponse.statusCode).toBe(401);
+        expect(reponse.body).toHaveProperty('message');
+        expect(reponse.body.message).toContain('Invalid credential');
+      });
+
+      it('should return user if creditional correct', async () => {
+        const userDto: UserCreateDto = {
+          email: 'test@gmail.com',
+          password: '1234567',
+        };
+        const reponse = await await request(BASE_URL)
+          .post('/auth/login')
+          .send(userDto);
+        token = reponse.body.access_token;
+        console.log(reponse.body);
+        expect(reponse.statusCode).toBe(201);
+        expect(reponse.body).toMatchObject({
+          access_token: expect.any(String),
+        });
+      });
+    });
+  });
+
+  describe('User', () => {
+    describe('get all users', () => {
+      it('should return users array with details', async () => {
+        const reponse = await request(BASE_URL).get('/user');
+
+        // console.log(reponse.body);
+
+        // token = reponse.body.access_token;
+        console.log(reponse.body);
+        expect(reponse.statusCode).toBe(200);
+        expect(reponse.body).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              id: expect.any(String),
+              email: expect.any(String),
+              password: expect.any(String),
+              firstName: expect.any(String),
+              lastName: expect.any(String),
+              role: expect.any(String),
+            }),
+          ]),
+        );
+        // expect(reponse.body).toMatchObject({
+        //   access_token: expect.any(String),
+        // });
+      });
+    });
+
+    describe('get user by id', () => {});
+
+    describe('update user', () => {});
+
+    describe('delete user', () => {});
+  });
+
+  describe('Post', () => {
+    describe('create', () => {
+      // it('should create a post ', async () => {
+      //   const postDto: PostCreateDto = {
+      //     content: 'test',
+      //     title: 'test title',
+      //     category: 'test category',
+      //   };
+      //   const reponse = await await request(BASE_URL)
+      //     .post('/post')
+      //     .set('Authorization', `Bearer ${token}`)
+      //     .send(postDto);
+      //   console.log(reponse.body);
+      //   expect(reponse.statusCode).toBe(201);
+      //   // expect(reponse.body).toHaveProperty('message');
+      //   // expect(reponse.body.message).toContain('Unauthorized');
+      // });
+    });
+
+    describe('get all', () => {});
+
+    describe('get by id', () => {});
+
+    describe('update', () => {});
+
+    describe('delete', () => {});
   });
 });
